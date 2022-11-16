77e887e6e4efc705bce5f6e316453f690a615995
MrTus97 <mr.tus97@gmail.com>
2022-11-17 03:01:09 +0700

    Add migration table

---
+++ database/migrations/2022_11_16_185134_create_wallets_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('wallets', function (Blueprint $table) {
+            $table->tinyIncrements('id');
+            $table->string('name', 100)->unique();
+            $table->boolean('archived')->default(false);
+            $table->string('description', 255)->nullable();
+            $table->bigInteger('amount')->default(0);
+            
+            $table->timestamps();
+            $table->softDeletes();
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('wallets');
+    }
+};
---
+++ database/migrations/2022_11_17_025833_create_transactions_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->smallIncrements('id');
+            $table->string('name', 100);
+            $table->string('description', 255)->nullable();
+            $table->string('icon')->nullable();
+            // 0 : Income, 1 Outcome, 2: own
+            $table->unsignedTinyInteger('type')->default(0);
+            $table->foreignId('wallet_id')->constrained();
+            $table->foreignId('parrent_id')->constrained('groups')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+};
