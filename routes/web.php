84660d380e5b35b00d70c11aeddfa085c63bf0d6
MrTus97 <mr.tus97@gmail.com>
2022-11-25 02:03:18 +0700

    Add authentication

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,8 @@
 <?php
 
-use App\Http\Controllers\Admin\ProductsController;
-use App\Models\User;
-use Illuminate\Http\Request;
+use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\HomeController;
-use App\Http\Controllers\CategroriesController;
+use Inertia\Inertia;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,80 +14,18 @@ use App\Http\Controllers\CategroriesController;
 | contains the "web" middleware group. Now create something great!
 |
 */
-// Route::get('/', function (){
-//     $html = "<h1> Học lập trình </h1>";
-//     return $html;
-// })->name('home');
-// Route::get('/', 'App\Http\Controllers\HomeController@index');
-// Route::get('/get-new', 'App\Http\Controllers\HomeController@getNews');
 
-// Route::get('category/{id}', [HomeController::class,'getCategory']);
+Route::get('/', function () {
+    return Inertia::render('Welcome', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
+    ]);
+});
 
-// Route::get('/unicode', function (){
-//     return view('form');
-// });
+Route::get('/dashboard', function () {
+    return Inertia::render('Dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-// Route::post('/unicode', function (){
-//     return 'Đây là unicode';
-// });
-// Route::put('/unicode', function (){
-//     return 'Đây là put unicode';
-// });
-
-// Route::match(['get', 'post'], 'unicode', function() {
-//     return $_SERVER['REQUEST_METHOD'];
-// });
-
-// Route::any('unicode', function (Request $request) {
-//     return $request->method();
-// });
-
-// Route::get('/show-form', function () {
-//     return view('form');
-// });
-
-// Route::redirect('unicode', 'show-form', 302);
-
-// Route::view('show-form', 'form');
-// Route::prefix('admin')->group(function () {
-//     Route::get('/tintuc/{slug}-{id?}', function ($slug, $id = null) {
-//         $content = 'Đây là phương thức lấy theo tham số: ';
-//         $content .= 'id = ' . $id;
-//         return $content;
-//     })->where(
-//         [
-//             'slug' => '[a-z]',
-//             'id' => '[0-9]+',
-//         ]
-//     );
-
-//     Route::get('/show-form', function () {
-//         return view('form');
-//     });
-// });
-
-// Client Route
-// Route::prefix('category')->group(function () {
-//     // Danh sách chuyên mục
-//     Route::get('/', [CategroriesController::class, 'index']);
-
-//     // Lấy chi tiết một chuyên mục
-//     Route::get('/edit/{id?}', [CategroriesController::class, 'showForm']);
-
-//     Route::post('/add', [CategroriesController::class, 'add'])->name('category.add');
-//     Route::get('/add', [CategroriesController::class, 'showForm']);
-// });
-
-// Route::middleware('auth.admin')->prefix('admin')->group(function () {
-//     Route::resource('products', ProductsController::class);
-// });
-
-// Route::prefix('admin')->group(function () {
-//     Route::resource('products', ProductsController::class);
-// });
-
-Route::get('create', [ProductsController::class, 'create']);
-
-Route::post('store', [ProductsController::class, 'store']);
-
-Route::get('show', [ProductsController::class, 'index']);
+require __DIR__.'/auth.php';
